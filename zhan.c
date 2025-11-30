ae75d8ac0a1ea244255c46479f74882ba668301c
2235425-7 <ws2305141819@qq.com>
2025-11-30 21:45:42 +0800

    添加栈销毁功能

--- zhan.c
+++ zhan.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define error -1
 #define MAXSIZE 100
 #define overflow -2
 #define ok 1
@@ -21,8 +22,24 @@ int initstack(sqstack *s){
     return ok;
 }
 
+int destorystack(sqstack *s){
+    if(s->base){
+        free(s->base);
+        s->stacksize = 0;
+        s->base = NULL;
+        s->top =NULL;
+        printf("栈销毁成功\n");
+        fflush(stdout);
+        return ok;
+    }
+    printf("销毁失败\n");
+    return error;
+}
+
 int main (){
     sqstack stack;
     initstack(&stack);
+    destorystack(&stack);
+    system("pause");
     return 0;
 }
