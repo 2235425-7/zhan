d100e1067310605b298dc90ea1ca8644b4f3f17d
2235425-7 <ws2305141819@qq.com>
2025-12-02 12:28:36 +0800

    顺序栈功能

--- zhan.c
+++ zhan.c
@@ -16,11 +16,42 @@ int initstack(sqstack *s){
     s->base = (selemtype*) malloc(MAXSIZE*sizeof(selemtype));
 
     if(!s->base) exit(overflow);  
-    s->base = s->top;
+    s->top = s->base;
     s->stacksize = MAXSIZE;
     printf("栈初始化完成\n");
     return ok;
 }
+int push(sqstack *s,selemtype e){
+    if(s->top-s->base == s->stacksize){
+        printf("栈满\n");
+        return error;
+    }
+    *s->top=e;
+    s->top++;
+        return ok;
+}
+int pop(sqstack *s,selemtype *e){
+    if(s->top == s->base){
+        printf("栈空\n");
+        return error;
+    }
+    s->top--;
+    *e=*s->top;
+        return ok;
+}
+void printstack(sqstack *s) {
+     if (s->base == s->top) {
+         printf("栈为空\n");
+         return;
+     }
+     printf("栈中的元素：");
+     selemtype *p = s->base;
+     while (p < s->top) {
+         printf("%d ", *p);
+         p++;
+     }
+     printf("\n");
+ }
 
 int destorystack(sqstack *s){
     if(s->base){
@@ -29,7 +60,6 @@ int destorystack(sqstack *s){
         s->base = NULL;
         s->top =NULL;
         printf("栈销毁成功\n");
-        fflush(stdout);
         return ok;
     }
     printf("销毁失败\n");
@@ -38,8 +68,20 @@ int destorystack(sqstack *s){
 
 int main (){
     sqstack stack;
+    selemtype e;
     initstack(&stack);
+    push(&stack,10);
+    push(&stack,20);
+    printf("入栈前\n");
+    printstack(&stack);
+    pop(&stack,&e);
+    printf("出栈的元素：%d\n",e);
+    printf("出栈后\n");
+    printstack(&stack);
     destorystack(&stack);
-    system("pause");
+    printstack(&stack);
+    printf("按任意键继续\n");
+    getchar();
+    getchar();
     return 0;
 }
